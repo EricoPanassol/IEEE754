ace5e087ec70dacf048922ac230dc313b284044c
EricoPanassol <ericopanassol@gmail.com>
2023-08-13 18:21:37 -0300

    [Feature] all files

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# IEEEE754
+# IEEE754
+
+## Disciplina: Métodos Numéricos
+Estudo e construção de modelos matemáticos na solução de problemas computacionais. Utilização adequada da aritmética de ponto flutuante. Análise dos fundamentos matemáticos de algoritmos numéricos. Identificação dos problemas de implementações em ponto flutuante. Apresentação dos conceitos que sustentam algoritmos numéricos e aplicação desses conceitos para a computação numérica eficiente. Realização de computação científica através da programação de algoritmos numéricos.
+
+
+### Trabalho 1
+
+Seu primeiro trabalho nesta disiplina onsiste em explorar o padrão IEEE-754. Voê deve
+implementar um programa que cumpra as seguintes tarefas:
+1. Ele reebe pela linha de comando uma expressão no formato
+val1 op val2
+onde op é uma operação (+, −, x, /) e val1 e val2 são dois valores em ponto flutuante, NaN
+ou ±∞.
+2. Depois de receber a expressão seu programa deve realizar a operação op e mostrar o resultado
+dela;
+3. Seu programa também deve mostrar a configuração de bits das duas variáveis e do resultado. Tome cuidado com a endianness do seu proessador para que a saída seja apresentada
+corretamente;
+4. Seu programa também deve informar se alguma exceção do padrão IEEE-754 foi sinalizada
+quando a operação foi feita. Use o registrador de status!
+Por exemplo, seu programa poderia funionar assim:
+
+```python
+python3 caluleitor.py 21 / -0
+
+Recebi 21.000000 / -0.000000 e resultado deu -inf
+val1 = 0 10000011 01010000000000000000000 = 21
+val2 = 1 00000000 00000000000000000000000 = -0
+res = 1 11111111 00000000000000000000000 = -inf
+Exceção FE_INEXACT: 0
+Exceção FE_DIVBYZERO: 1
+Exceção FE_UNDERFLOW: 0
+Exceção FE_OVERFLOW: 0
+Exceção FE_INVALID: 0
+```
+Atenção: faça este trabalho em C. Não faça em Python, não faça em Java. Aqui estão (apenas
+alguns) motivos:
+Da especifação da máquina virtual java:
+
+<em>The floating-point operations of the Java Virtual Mahine do not throw
+exceptions, trap, or otherwise signal the IEEE 754 exceptional conditions
+of invalid operation, division by zero, overflow, underflow, or inexact.
+The Java Virtual Mahine has no signaling NaN value.
+</em>
+
+Sobre Python: você pode ter alguma esperança usando numpy e numpy.seterr, mas vai estar
+correndo riscos. A responsabilidade é toda sua e a chance de arrependimento é alta.
+
+### Utilização
+
+Para inicar o programa, basta executar o seguinte comando no terminal:
+
+```bash
+gcc -o ieee754 ieee754.c -lm 
+```
+
+Após o programa ser compilado, basta executar o seguinte comando no terminal:
+
+```bash
+./ieee754 val1 op val2
+```
---
+++ ieee754.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>  
+#include <fenv.h>
+#include <math.h>
+
+
+// Função para imprimir a representação binária de um número em ponto flutuante
+void convertToBinaryRep(float value) {
+    uint32_t *ptr = (uint32_t *)&value;
+    uint32_t bits = *ptr;
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (bits >> i) & 1);
+        if (i == 31 || i == 23) printf(" ");
+    }
+}
+
+
+// Função para realizar a operação e mostrar os resultados no padrão IEEE 754
+void performOperation(float val1, char op, float val2) {
+    float result;
+
+    feclearexcept(FE_ALL_EXCEPT); // Limpa as exceções pendentes
+
+    switch (op) {
+        case '+':
+            result = val1 + val2;
+            break;
+        case '-':
+            result = val1 - val2;
+            break;
+        case 'x':
+            result = val1 * val2;
+            break;
+        case '/':
+            result = val1 / val2;
+            break;
+        default:
+            printf("Invalid operation\nValids: +, -, x, /\n");
+            return;
+    }
+
+    // Imrpime os valores e o resultado da operação
+    printf("Result %.6f %c %.6f = %.6f\n\n", val1, op, val2, result);
+
+    printf("val1 = ");
+    convertToBinaryRep(val1);
+    printf(" = %.6f", val1);
+    printf("\n");
+
+    printf("val2 = ");
+    convertToBinaryRep(val2);
+    printf(" = %.6f", val2);
+    printf("\n");
+
+    printf("res = ");
+    convertToBinaryRep(result);
+    printf(" = %.6f", result);
+    printf("\n");
+    printf("\n");
+
+    // Verifica se ocorreu alguma exceção E qual foi, se sim, ativa a flag
+    if (fetestexcept(FE_INEXACT)) {
+        printf("Exception FE_INEXACT: 1\n");
+    } else {
+        printf("Exception FE_INEXACT: 0\n");
+    }
+
+    if (fetestexcept(FE_DIVBYZERO)) {
+        printf("Exception FE_DIVBYZERO: 1\n");
+    } else {
+        printf("Exception FE_DIVBYZERO: 0\n");
+    }
+
+    if (fetestexcept(FE_UNDERFLOW)) {
+        printf("Exception FE_UNDERFLOW: 1\n");
+    } else {
+        printf("Exception FE_UNDERFLOW: 0\n");
+    }
+
+    if (fetestexcept(FE_OVERFLOW)) {
+        printf("Exception FE_OVERFLOW: 1\n");
+    } else {
+        printf("Exception FE_OVERFLOW: 0\n");
+    }
+
+    if (fetestexcept(FE_INVALID)) {
+        printf("Exception FE_INVALID: 1\n");
+    } else {
+        printf("Exception FE_INVALID: 0\n");
+    }
+}
+
+
+// Função principal que recebe os argumentos pela linha de comando e chama a função performOperation
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s val1 op val2\n", argv[0]);
+        return 1;
+    }
+
+    float val1 = strtof(argv[1], NULL);
+    char op = argv[2][0];
+    float val2 = strtof(argv[3], NULL);
+
+    performOperation(val1, op, val2);
+
+    return 0;
+}

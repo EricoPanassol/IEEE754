d58ee13351887f845fed74ac6aad7583122ec578
Érico Panazzolo <91470140+EricoPanassol@users.noreply.github.com>
2023-08-16 21:21:47 -0300

    [Feature] adding more numbers after the column

--- ieee754.c
+++ ieee754.c
@@ -15,12 +15,6 @@ void convertToBinaryRep(float value) {
     }
 }
 
-
-// addition
-// subtraction
-// multiplication
-// division
-
 // Função para realizar a operação, mostrar os resultados no padrão IEEE 754 
 // e checar se alguma flag foi ativada para então, liga-lá
 void performOperation(float val1, char op, float val2) {
@@ -50,28 +44,21 @@ void performOperation(float val1, char op, float val2) {
     }
 
     // Imrpime os valores e o resultado da operação
-   
-    // if(result == INFINITY){
-    //     printf("Result %.6f %c %.6f = INF\n\n", val1, op, val2);
-    // } else {
-    //     printf("Result %.6f %c %.6f = %.6f\n\n", val1, op, val2, result);
-    // }
-    
-    printf("Result = %.6f %c %.6f = %.6f\n\n", val1, op, val2, result);
+    printf("Result = %.26f %c %.26f = %.26f\n\n", val1, op, val2, result);
 
     printf("val1 = ");
     convertToBinaryRep(val1);
-    printf(" = %.20f", val1);
+    printf(" = %.26f", val1);
     printf("\n");
 
     printf("val2 = ");
     convertToBinaryRep(val2);
-    printf(" = %.20f", val2);
+    printf(" = %.26f", val2);
     printf("\n");
 
     printf("res = ");
     convertToBinaryRep(result);
-    printf(" = %.20f", result);
+    printf(" = %.26f", result);
     printf("\n");
     printf("\n");
 
